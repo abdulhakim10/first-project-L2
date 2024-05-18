6c6af139831a06d3c657ad97ad76f5c1d2570719
abdulhakim10 <s.abdulhakim10@gmail.com>
2024-05-18 16:58:42 +0800

    route, controller and service file created for moduler pattern

---
+++ src/app/modules/students/student.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { StudentServices } from "./student.service";
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const student = req.body;
+    // will call service function to send this data
+    const result = await StudentServices.createStudentIntoDB(student);
+
+    res.status(201).json({
+      success: true,
+      message: "Student created succesfully",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const StudentControllers = {
+  createStudent,
+};
---
+++ src/app/modules/students/student.route.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import { StudentControllers } from "./student.controller";
+
+const router = express.Router();
+
+// will call cntroller function
+router.post("/create-student", StudentControllers.createStudent);
