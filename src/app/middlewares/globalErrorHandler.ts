1f3802d08e34bd2ba1cbe27c805ce11c43eb4c72
abdulhakim10 <s.abdulhakim10@gmail.com>
2024-06-24 12:18:29 +0800

    error handler added for mongoose

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,16 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
-import { TErrorSource } from "../interface/error";
+import { TErrorSources } from "../interface/error";
 import { ZodError } from "zod";
 import config from "../config";
 import handleZodError from "../errors/handleZodError";
+import handleValidationError from "../errors/handleValidationError";
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let statusCode = err.statusCode || 500;
   let message = err.message || "Something went wrong";
 
-  let errorSources: TErrorSource = [
+  let errorSources: TErrorSources = [
     {
       path: "",
       message: "Something went wrong",
@@ -19,15 +20,20 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
 
   if (err instanceof ZodError) {
     const simplifiedError = handleZodError(err);
-    statusCode = simplifiedError.statusCode;
-    message = simplifiedError.message;
-    errorSources = simplifiedError.errorSources;
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === "ValidationError") {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
   }
-
   return res.status(statusCode).json({
     success: false,
     message,
     errorSources,
+    // err,
     stack: config.NODE_ENV === "development" ? err?.stack : null,
     // error: err,
   });
