5e5fa5cd7a1b9f9ab92dc61cc27433e7f1e4c5ea
abdulhakim10 <s.abdulhakim10@gmail.com>
2024-08-14 10:07:30 +0800

    course interface and model are created

---
+++ src/app/modules/course/course.interface.ts
@@ -0,0 +1,14 @@
+import { Types } from "mongoose";
+
+export type TPreRequisiteCourses = {
+  course: Types.ObjectId; // it's actually (id) for ease course written here
+  isDeleted: boolean;
+};
+
+export type TCourse = {
+  title: string;
+  prefix: string;
+  code: number;
+  credits: number;
+  preRequisiteCourses: [];
+};
